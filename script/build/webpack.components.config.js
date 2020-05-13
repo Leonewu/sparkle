f50d768433dc0b54a23bd216e2a0b1b509d1eb95
leone <379747139@qq.com>
2020-05-13 11:31:11 +0800

    chore: stylelint

--- script/build/webpack.components.config.js
+++ script/build/webpack.components.config.js
@@ -3,6 +3,11 @@ const VueLoaderPlugin = require('vue-loader/lib/plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 // TODO 两份配置差异不大可以通过 merge 来合并
+// 规则：
+// 全量打包： css 样式通过 style-loader 嵌入 dom 中
+// 按需加载打包：每个组件作为一个入口打包，不用 style-laoder ，通过 miniCssExtractPlugin 拆分各自的 css
+// 并且提取全局的 css 到 base.css
+// 打包出来的目录结构尽量按照 babel-plugin-component 的默认路径
 
 module.exports = {
   mode: 'development',
@@ -17,6 +22,11 @@ module.exports = {
     // 打包成 umd
     libraryTarget: 'umd'
   },
+  // optimization: {
+  //   minimizer: [
+
+  //   ]
+  // },
   module: {
     rules: [
       {
