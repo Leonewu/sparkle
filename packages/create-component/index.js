16757ebd5dc18b6f167498b29fd8d7b4e90d33f9
leone <379747139@qq.com>
2020-05-15 18:53:29 +0800

    :construction: create component cli

---
+++ packages/create-component/index.js
@@ -0,0 +1,9 @@
+#! /usr/bin/env node
+// const glob = require('glob')
+
+// glob("**/*.js", {}, function(er, files) {
+//   // files is an array of filenames.
+//   // If the `nonull` option is set, and nothing
+//   // was found, then files is ["**/*.js"]
+//   // er is an error object or null.
+// })
