af9a914176f577e87bcfdc5f8da32ecc30f05a40
吴高亮 <leone@DESKTOP-6S99CEJ.localdomain>
2020-05-21 15:48:14 +0800

    :zap: update docs and optimize src/index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 import Button from './components/button/button.vue'
 import Select from './components/select/select.vue'
 
-const components = {
-  // 组件名: 组件实例
-  'xiao-button': Button,
-  'xiao-select': Select
-}
+const components = [Button, Select]
 
 const install = function(Vue) {
-  for (const name in components) {
-    Vue.component(name, components[name])
-  }
+  components.forEach((component) => {
+    Vue.use(component)
+  })
 }
 
 export default {
