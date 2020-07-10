f20f086ff8d982371434b1afbcbaae309226eb46
leone <leone@leonedeMac-mini.local>
2020-07-10 21:38:39 +0800

    :paw_prints: chore: write a simple glob.js for this project

--- packages/compile/utils/cache.js
+++ packages/compile/utils/cache.js
@@ -1,7 +1,7 @@
 const fs = require('fs-extra')
 const path = require('path')
-// 经常会用到查看文件是否存在
-// 缓存起来
+// 大量使用 fs.existSync
+// 所以将结果缓存起来
 // 要注意创建文件的时候需要刷新缓存
 
 class File {
@@ -38,46 +38,64 @@ function updateCache(filePath) {
   delete cache[filePath]
   isExist(filePath)
 }
+// 低配版 glob
 // home/src/**/*.{scss,less,styl,css}
 // 只支持 ** 和 * 的语法，如上
+// 思路：
+// 1. 确定入口 entry，即 * 之前的路径
+// 2. 将除入口之外，剩余的路径按照 / 分割
+// 3. 分割出来的数组 routes，假设 routes 有两层，就生成一个相同长度的数组 results
+// 4. 遍历 routes，从入口 entry 开始，将每一层的结果塞进 results 相同层的数组中
+// 5. 遍历过程中，从第二层开始的父路径，都从 results 拿
+// 6. 遍历结束，从结果 results 中拿出最后一层的结果，再根据 .{vue} 之类的条件过滤
 function glob(str) {
   let files = []
-    // 目录
+  // 最后的结果是否为目录
   const isDir = str.substr(-1) === '/'
-  let prefix = [str.match(/([^*]+)/)[1]]
-  let left = str.replace(prefix, '').split('/')
-  console.log(left)
-  while (prefix.length) {
-    const curPrefix = prefix.shift()
-    for (let i = 0; i < left.length; i++) {
-      const curStr = left[i]
-      if (curStr === '') {
-        if (i === left.length - 1) {
-          // 如果是最后一个并且为空，说明是目录
-          const dirs = prefix.filter(d => fs.lstatSync(d).isDirectory())
-          files.push(...dirs)
-          prefix = []
-        }
-      } else if (curStr === '*') {
-        const res = fs.readdirSync(curPrefix).map(s => `${curPrefix}${s}`)
-        if (i !== left.length - 1) {
-          // 不是最后一个，说明这个是目录
-          const dirs = res.filter(d => fs.lstatSync(d).isDirectory())
-          prefix.push(...dirs)
-          left.shift()
-        } else {
-          files.push(...res)
-        }
-        break
-      }
-    }
+  const entry = str.match(/([^*]+)\//)[1]
+  let filename = ''
+  const routes = str.replace(entry, '').split('/')
+  if (routes[routes.length - 1] && routes[routes.length - 1].includes('.')) {
+    // 有后缀
+    fileName = routes.pop()
   }
+  routes[0] === '' && routes.shift()
+  routes[routes.length - 1] === '' && routes.pop()
+  const results = routes.map(() => [])
+  routes.forEach((route, index) => {
+    let res = []
+    if (route === '*') {
+      if (index === 0) {
+        if (index === routes.length - 1 && !isDir) {
+          // 刚好第一层就是最后一层，并且不用筛出目录
+          res = fs.readdirSync(entry).map(s => `${entry}/${s}`)
+        } else {
+          res = fs.readdirSync(entry).map(s => `${entry}/${s}`)
+          res = res.filter(p => fs.lstatSync(p).isDirectory())
+        }
+      } else {
+        // 不是第一个 *
+        results[index - 1].forEach(parent => {
+          const sub = fs.readdirSync(parent).map(s => `${parent}/${s}`)
+          if (index < routes.length - 1 || isDir) {
+            // 过滤出文件夹
+            res = res.concat(sub.filter(p => fs.lstatSync(p).isDirectory()))
+          } else {
+            res = res.concat(...sub)
+          }
+        })
+      }
+      results[index].push(...res)
+    } else if (route === '**') {
 
+    }
+  })
+  files = results[results.length - 1]
   return files
 }
 // console.log(glob('/home/leone/lib/*/'))
 // console.log(glob('/home/leone/lib/*'))
-console.log(glob('/home/leone/lib/xiao-ui/*'))
+console.log(glob('/Users/leone/leone/test/src/select/*/index.{vue,jsx,tsx}'))
 module.exports = {
   isExist
 }
