3bc6cd7b5ae1813c7609713517f93d0b4735ce0c
leone <leone@DESKTOP-6S99CEJ.localdomain>
2020-07-18 17:23:42 +0800

    :shit: chore: optimize build queue

--- packages/compile/codegen/generate-css-module.js
+++ packages/compile/codegen/generate-css-module.js
@@ -1,5 +1,5 @@
 const fs = require('fs-extra')
-const { BASE_STYLE_FILE, COMPONENTS, OUTPUT_DIR, STYLE_EXT } = require('../config')
+const { BASE_STYLE_FILE, COMPONENTS, ES_DIR, STYLE_EXT } = require('../config')
 const path = require('path')
 const { getDeps } = require('../deps')
 const { getPreStyle } = require('../utils/')
@@ -12,19 +12,19 @@ function generateCssModule() {
       COMPONENTS.forEach(component => {
         let sassImports = `import "../../${BASE_STYLE_FILE}"`
         const deps = getDeps(component)
-        const styleFile = getPreStyle(`${OUTPUT_DIR}/${component}/`)
+        const styleFile = getPreStyle(`${ES_DIR}/${component}/`)
         if (styleFile.path) {
           sassImports += `\nimport "../${styleFile.ext}"`
         }
         deps.forEach(dep => {
-          const preStyleFile = getPreStyle(path.join(OUTPUT_DIR, component, dep.path))
+          const preStyleFile = getPreStyle(path.join(ES_DIR, component, dep.path))
           if (preStyleFile.path) {
             const relativePath = path.join('..', `${dep.path}${preStyleFile.ext}`)
             sassImports += `\nimport "${relativePath}"`
           }
         })
         const cssImports = sassImports.replace(new RegExp(`${STYLE_EXT}`, 'g'), '.css')
-        const styleModuleDir = `${OUTPUT_DIR}/${component}/style`
+        const styleModuleDir = `${ES_DIR}/${component}/style`
         fs.outputFileSync(`${styleModuleDir}/${STYLE_EXT.substr(1)}.js`, sassImports)
         fs.outputFileSync(`${styleModuleDir}/css.js`, cssImports)
       })
--- packages/compile/compile-style.js
+++ packages/compile/compile-style.js
@@ -1,12 +1,12 @@
 
 const compileSass = require('./sass-compiler')
 const fs = require('fs-extra')
-const { OUTPUT_DIR, STYLE_EXT } = require('./config')
+const { ES_DIR, STYLE_EXT } = require('./config')
 const { cacheGlob: glob } = require('./utils/glob')
 
 function compileStyle() {
   // 编译所有样式文件
-  const styleFiles = glob(`${OUTPUT_DIR}/**/*.{${STYLE_EXT.substr(1)}, css}`)
+  const styleFiles = glob(`${ES_DIR}/**/*.{${STYLE_EXT.substr(1)}, css}`)
   const promises = styleFiles.map(filePath => {
     return new Promise(async (resolve, reject) => {
       try {
